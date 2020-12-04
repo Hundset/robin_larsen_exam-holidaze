d108e0a82985db67fd6809064d52a5d902da4cea
Robin S Bartlett Larsen <hundset@gmail.com>
2020-12-04 11:23:53 +0100

    Updates

---
+++ precache-manifest.21da0a7f4960506c67c877bbe41b95d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e8b97e046dbe021933844898729ad6",
+    "url": "/robin_larsen_exam-holidaze/index.html"
+  },
+  {
+    "revision": "60c3c105bead71d79205",
+    "url": "/robin_larsen_exam-holidaze/static/css/main.bbfb8d06.chunk.css"
+  },
+  {
+    "revision": "2920748ed7ed87bd7696",
+    "url": "/robin_larsen_exam-holidaze/static/js/2.1cc3587a.chunk.js"
+  },
+  {
+    "revision": "fff215cbdd8bf6c76ea43e336155859d",
+    "url": "/robin_larsen_exam-holidaze/static/js/2.1cc3587a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c3c105bead71d79205",
+    "url": "/robin_larsen_exam-holidaze/static/js/main.bbf79bb8.chunk.js"
+  },
+  {
+    "revision": "a6b8e7b05354a7b93001",
+    "url": "/robin_larsen_exam-holidaze/static/js/runtime-main.bef17b53.js"
+  },
+  {
+    "revision": "fd41d56cdae2b643a6f1b1faa34cceb8",
+    "url": "/robin_larsen_exam-holidaze/static/media/Copperplate.fd41d56c.ttc"
+  },
+  {
+    "revision": "fcc80f51f39035aa20681b5435b21110",
+    "url": "/robin_larsen_exam-holidaze/static/media/leonard-rb-S-gj2BlwWjU-unsplash.fcc80f51.jpg"
+  }
+]);
